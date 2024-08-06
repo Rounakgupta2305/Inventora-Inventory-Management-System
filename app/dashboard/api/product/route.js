ae1f47c58c3591ccae733dab7f5e762b488dd4f4
Rounak Gupta <Rounak.1si21ec080@gmail.com>
2024-08-07 03:56:36 +0530

    Update route.js

--- app/dashboard/api/product/route.js
+++ app/dashboard/api/product/route.js
@@ -3,30 +3,37 @@ import { NextResponse } from "next/server";
 import { getAuth } from "@clerk/nextjs/server";
 
 export async function GET(request) {
-  const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
-  const { userId } = getAuth(request);
-  const client = new MongoClient(uri);
-  try {
-    const database = client.db('stock');
-    const inventory = database.collection('inventory');
-    const query = { userId };
-    const products = await inventory.find(query).toArray();
-    return NextResponse.json({ success: true, products })
-  } finally {
-    await client.close();
-  }
-
-}
-
-export async function POST(request) {
-  let body = await request.json()
+  const query = request.nextUrl.searchParams.get("query");
   const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
   const client = new MongoClient(uri);
+
   try {
+    const { userId } = getAuth(request); // Get the current user ID
+
+    if (!userId) {
+      return NextResponse.json({ success: false, message: "User not authenticated" });
+    }
+
     const database = client.db('stock');
     const inventory = database.collection('inventory');
-    const product = await inventory.insertOne(body)
-    return NextResponse.json({ product, ok: true })
+
+    const products = await inventory.aggregate([
+      {
+        $match: {
+          userId,
+          $or: [
+            { brandName: { $regex: query, $options: "i" } },
+            { productName: { $regex: query, $options: "i" } },
+            { modelName: { $regex: query, $options: "i" } },
+          ]
+        }
+      }
+    ]).toArray();
+
+    return NextResponse.json({ success: true, products });
+  } catch (error) {
+    console.error('Error during search operation:', error);
+    return NextResponse.json({ success: false, message: 'Internal server error' });
   } finally {
     await client.close();
   }
