bbda0801b0d8932d6873042884747dcd6bf3769e
Rounak Gupta <Rounak.1si21ec080@gmail.com>
2024-08-07 03:56:21 +0530

    Update route.js

--- app/dashboard/api/product/route.js
+++ app/dashboard/api/product/route.js
@@ -1,13 +1,15 @@
 import { MongoClient } from "mongodb";
 import { NextResponse } from "next/server";
+import { getAuth } from "@clerk/nextjs/server";
 
 export async function GET(request) {
   const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
+  const { userId } = getAuth(request);
   const client = new MongoClient(uri);
   try {
     const database = client.db('stock');
     const inventory = database.collection('inventory');
-    const query = {};
+    const query = { userId };
     const products = await inventory.find(query).toArray();
     return NextResponse.json({ success: true, products })
   } finally {
